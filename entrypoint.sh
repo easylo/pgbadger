4aba444f501310a5b010d92a4e652a13e9d84a6f
Laurent RICHARD <lrichard@lafourchette.com>
2019-03-14 14:37:12 +0100

    add cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,45 @@
 
 set -euo pipefail
 
+PGBADGER_PREFIX="${PGBADGER_PREFIX:-' '}"
+
 if [ "$1" != "pgbadger" ]; then
-  set -- pgbadger "$@"
+  set -- pgbadger "$@"   
 fi
+
+# if [ ! -z "$PGBADGER_PREFIX" -a "$PGBADGER_PREFIX" != " " ]; then
+#       # PGBADGER_PREFIX=" $PGBADGER_PREFIX"
+#       # "${@/pgbadger/$PGBADGER_PREFIX}"
+#       # set -- "pgbadger " "$@" 
+    
+# fi
 
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 export AWS_DEFAULT_REGION=$AWS_S3_REGION
 
+export nproc=4
+
 mkdir -p "$PGBADGER_DATA"
 
 upload_s3 () {
   aws s3 cp --recursive $1/ s3://$2/ --include "*.html" --include "*.json" --exclude "*.bin" --exclude "*.DS_Store" --exclude "*LAST_PARSED"
-  return 1
+  return 0
 }
 
 run_pgbadger () {
 
-  DATE=`date +%Y-%m-%d`
 
-  $1 --jobs $(nproc) --outdir "$PGBADGER_DATA" --incremental --start-monday --outfile "index.html"
-  $1 --jobs $(nproc) --outdir "$PGBADGER_DATA" --outfile "index_$DATE.json" --extension json
-  return 1
+  $@ --prefix "$PGBADGER_PREFIX" --jobs $nproc --outdir "$PGBADGER_DATA" --disable-temporary --incremental --start-monday --outfile "index.html"
+  # DATE=`date +%Y/%m/%d`
+  # mkdir -p "$PGBADGER_DATA/${DATE}"
+  # $@ --prefix "$PGBADGER_PREFIX" --jobs $nproc --disable-temporary  --start-monday --outfile "$PGBADGER_DATA/${DATE}/index.json" --extension json --prettify-json
+
+  return 0
 }
+
+
+
 
 if [[ 1 == "${CRON}" ]]; then
 
